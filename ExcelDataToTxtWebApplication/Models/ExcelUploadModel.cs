b6812b0182e608958f77aec84278a42fca726ede
Магомедали Гаджиев <mag198421@gmail.com>
2025-02-17 20:01:27 +0300

    Модель для загрузки ExcelUploadModel

---
+++ ExcelDataToTxtWebApplication/Models/ExcelUploadModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ExcelDataToTxtWebApplication.Models
+{
+    public class ExcelUploadModel
+    {
+        public HttpPostedFileBase ExcelFile { get; set; }
+        public string Message { get; set; }
+        public List<string> GeneratedFiles { get; set; } = new List<string>();
+    }
+}
