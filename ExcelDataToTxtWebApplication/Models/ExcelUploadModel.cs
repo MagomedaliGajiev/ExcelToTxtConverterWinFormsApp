3d469c59456fdebbc469ef4d3613edf97d4b57b9
Магомедали Гаджиев <mag198421@gmail.com>
2025-02-17 21:31:14 +0300

    Определенные изменения

--- ExcelDataToTxtWebApplication/Models/ExcelUploadModel.cs
+++ ExcelDataToTxtWebApplication/Models/ExcelUploadModel.cs
@@ -1,8 +1,11 @@
-﻿namespace ExcelDataToTxtWebApplication.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExcelDataToTxtWebApplication.Models
 {
     public class ExcelUploadModel
     {
-        public HttpPostedFileBase ExcelFile { get; set; }
+        [Required(ErrorMessage = "Выберите файл Excel")]
+        public IFormFile ExcelFile { get; set; }
         public string Message { get; set; }
         public List<string> GeneratedFiles { get; set; } = new List<string>();
     }
